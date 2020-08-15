79fb9180c66f4a297638262cad64a2525ffc3f41
Sable <sablechilds@yahoo.com>
2020-08-14 23:29:21 -0400

    register-endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+const jwt = require('jsonwebtoken');
+
+const bcrypt = require('bcryptjs');
+
+router.post('/register', async (req, res) => {
+  
+  const user = req.body
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash;
+
+  try {
+    const newUser = await Users.add(user)
+    res.status(201).json(newUser, {message:''})
+  } catch(err) {
+    res.status(500).json({message:'unable to register user'})
+  }
+
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   // implement login
+  
 });
 
 module.exports = router;
